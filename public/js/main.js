18a9b695c70931cbbd1bcf2acf48049cf1fbfae6
Igor Wysocki <lgorvysocky@gmail.com>
2024-03-22 00:07:31 +0300

    txt files data, load data

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,23 @@
 function onLoad() {
+    loaderFor(document.getElementById('container'));
+    setTimeout(content, 2000);
+}
+
+function content() {
+    const fTracks = () => {
+        const bolds = tracks.map((el) => `<b>${el}</b>`);
+        const joined = bolds.join(' • ');
+        return `<p id="tracks">${joined}</p>`
+    }
+
+    const fWords = () => {
+        return `<p id="words">${words}</p>`;
+    }
+
+    const container = document.getElementById('container');
+    container.innerHTML = fTracks() + fWords();
+}
+
+function loaderFor(element) {
+    element.innerText = '...';
 }
