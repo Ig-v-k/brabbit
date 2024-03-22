95d6758b0dab0fd3ec51a920e012884c2911fb2b
Igor Wysocki <lgorvysocky@gmail.com>
2024-03-22 22:50:22 +0300

    filter empty strings

--- public/js/words.js
+++ public/js/words.js
@@ -1,6 +1,6 @@
 function counts(text) {
     const symbols = /[.,\/#!$%\^&\*;:{}=\-_`~()]/g;
-    const words = text.replace(symbols, "").split(" ");
+    const words = text.replace(symbols, "").split(" ").filter(word => word.trim());
     const counts = {};
     words.forEach(word => {
         word = word.trim();
